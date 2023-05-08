9f997f7f74f7a2bdcb6b875fd0b40088c20f056e
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-08 11:47:11 +0600

    home page update

--- lib/provider/home_provider.dart
+++ lib/provider/home_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/http/custom_http.dart';
-import 'package:news_app/model/news_model.dart';
 
-class HomeProvider with ChangeNotifier{
+import '../http/custom_http.dart';
+import '../model/news_model.dart';
+
+
+
+class NewsProvider with ChangeNotifier{
 
   NewsModel? newsModel;
   Future<NewsModel> getHomeData(int pageNo,String sortBy)async{
