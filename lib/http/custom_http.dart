8a93497f1e699173fadf8fcdcbb8a9c83f80bad5
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-08 11:48:09 +0600

    custom http page update

--- lib/http/custom_http.dart
+++ lib/http/custom_http.dart
@@ -1,16 +1,27 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import '../model/news_model.dart';
+
 
 class CustomeHttpRequest {
   static Future<NewsModel> fetchHomeData(int pageNo,String sortBy) async {
     String url =
         "https://newsapi.org/v2/everything?q=football&sortBy=$sortBy&pageSize=10&page=${pageNo}&apiKey=4c9e5047288c404d890710a4ff8dab2a";
+
     NewsModel? newsModel;
     var responce = await http.get(Uri.parse(url));
-    print("status code is ${responce.statusCode}");
     var data = jsonDecode(responce.body);
-    print("our responce is ${data}");
+    newsModel = NewsModel.fromJson(data);
+    return newsModel!;
+  }
+  static fetchSearchData(String query) async {
+    String url =
+        "https://newsapi.org/v2/everything?q=${query}&pageSize=20&apiKey=4c9e5047288c404d890710a4ff8dab2a";
+
+    NewsModel? newsModel;
+    var responce = await http.get(Uri.parse(url));
+    var data = jsonDecode(responce.body);
     newsModel = NewsModel.fromJson(data);
     return newsModel!;
   }
